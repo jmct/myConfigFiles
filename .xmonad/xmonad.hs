53df235dabeb70eab9f77f286f45c0c295676a36
Jose Calderon <jose+gitt@jmct.cc>
2011-09-25 16:01:10 -0400

    Got xmobar configured with systray now easily added

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,6 +1,19 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
 
-main = xmonad =<< xmobar myConfig
-myConfig = defaultConfig { modMask = mod1Mask
-                          ,terminal = "urxvt" }
+main = do
+    xmproc <- spawnPipe "/usr/bin/xmobar /home/jmct/.xmobarrc"
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts $ layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+                    { ppOutput = hPutStrLn xmproc
+                    , ppTitle = xmobarColor "red" "" . shorten 50
+                    }
+        , modMask = mod1Mask
+        , terminal = "urxvt"
+        }
